5ed0e09e2c14f0c46e84edb429238d20f423516d
ngomile <ngomile932@gmail.com>
2022-12-04 11:32:23 +0200

    Add class fields and methods for JobSearchMW source

--- lib/sources/jobsearchmalawi.dart
+++ lib/sources/jobsearchmalawi.dart
@@ -1,1 +1,22 @@
-class JobSearchMW {}
+import 'package:katswiri/models/job.dart';
+
+class JobSearchMW {
+  static const title = 'JobSearchMW';
+  static const host = 'jobsearchmalawi.com';
+
+  static final listingsUri = Uri.https(host, 'jm-ajax/get_listings');
+
+  Map<String, dynamic> get _headers => {
+        'Host': host,
+        'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Origin': 'https://jobsearchmalawi.com',
+        'Alt-Used': 'jobsearchmalawi.com',
+        'Referer': 'https://jobsearchmalawi.com/',
+      };
+
+  Future<Job> fetchJob(String url) async {}
+
+  Future<List<Job>> fetchJobs() async {}
+}
