2104e77261436c673a412b7a23f14ec7e0411df6
ngomile <ngomile932@gmail.com>
2022-12-04 11:14:44 +0200

    Implement job model

---
+++ lib/models/job.dart
@@ -0,0 +1,19 @@
+class Job {
+  const Job({
+    required this.logo,
+    required this.position,
+    required this.companyName,
+    required this.location,
+    required this.type,
+    required this.posted,
+    this.description = "",
+  });
+
+  final String logo;
+  final String position;
+  final String companyName;
+  final String location;
+  final String type;
+  final String posted;
+  final String description;
+}
