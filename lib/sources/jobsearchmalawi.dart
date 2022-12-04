e19efac176ffa41834c6dcfcb93b954da5d329d1
ngomile <ngomile932@gmail.com>
2022-12-04 11:55:35 +0200

    Implement fetchJob method of JobSearchMW source

--- lib/sources/jobsearchmalawi.dart
+++ lib/sources/jobsearchmalawi.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parseFragment;
+
 import 'package:katswiri/models/job.dart';
 
 class JobSearchMW {
@@ -6,7 +12,7 @@ class JobSearchMW {
 
   static final listingsUri = Uri.https(host, 'jm-ajax/get_listings');
 
-  Map<String, dynamic> get _headers => {
+  Map<String, String> get _headers => {
         'Host': host,
         'User-Agent':
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0',
@@ -16,7 +22,31 @@ class JobSearchMW {
         'Referer': 'https://jobsearchmalawi.com/',
       };
 
-  Future<Job> fetchJob(String url) async {}
+  Future<Job> fetchJob(String url) async {
+    final Job job;
+    final url = listingsUri;
+
+    final response = await http.post(url, headers: _headers);
+
+    if (response.statusCode != 200) {
+      throw HttpException('HTTP Request completed with ${response.statusCode}');
+    }
+
+    final json = jsonDecode(response.body);
+    final $ = parseFragment(json['html']);
+
+    job = Job(
+      logo: $.querySelector('.company_logo')?.attributes['src']?.trim() ?? '',
+      position: $.querySelector('.entry-title')?.text.trim() ?? '',
+      companyName: $.querySelector('p.name > strong')?.text.trim() ?? '',
+      location: $.querySelector('.location > a')?.text.trim() ?? '',
+      type: $.querySelector('li.job-type')?.text.trim() ?? '',
+      posted: $.querySelector('.date-posted > time')?.text.trim() ?? '',
+      description: $.querySelector('.job_description')?.text.trim() ?? '',
+    );
+
+    return job;
+  }
 
   Future<List<Job>> fetchJobs() async {}
 }
