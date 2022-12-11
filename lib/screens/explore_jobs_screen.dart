b8947c6355506dd3a47dbce139f6ad8f5ecff5c6
ngomile <ngomile932@gmail.com>
2022-12-11 03:39:05 +0200

    Implement tabbed navigation for sources and add _JobList widget to display list of job entries

--- lib/screens/explore_jobs_screen.dart
+++ lib/screens/explore_jobs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:katswiri/sources/sources.dart';
 
 class ExploreJobsScreen extends StatefulWidget {
   const ExploreJobsScreen({super.key});
@@ -9,11 +10,60 @@ class ExploreJobsScreen extends StatefulWidget {
   State<ExploreJobsScreen> createState() => _ExploreJobsScreenState();
 }
 
-class _ExploreJobsScreenState extends State<ExploreJobsScreen> {
+class _ExploreJobsScreenState extends State<ExploreJobsScreen>
+    with SingleTickerProviderStateMixin {
+  final _sources = getSources();
+
+  late final _tabController = TabController(
+    length: _sources.length,
+    vsync: this,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Explore Jobs'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Explore'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: _sources
+              .map<Widget>((source) => Tab(
+                    text: source.title,
+                  ))
+              .toList(),
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children:
+            _sources.map<Widget>((source) => _JobList(source: source)).toList(),
+      ),
+    );
+  }
+}
+
+class _JobList extends StatefulWidget {
+  const _JobList({super.key, required this.source});
+
+  final Source source;
+
+  @override
+  State<_JobList> createState() => __JobListState();
+}
+
+class __JobListState extends State<_JobList> {
+  late final Source _source;
+
+  @override
+  void initState() {
+    super.initState();
+    _source = widget.source;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(_source.title),
     );
   }
 }
