d85f60ff5bf8e1cdd70838087e60c10a81b27294
ngomile <ngomile932@gmail.com>
2022-12-11 02:15:31 +0200

    Add routes and routes to unimplemented screens

--- lib/screens/katswiri.dart
+++ lib/screens/katswiri.dart
@@ -3,24 +3,44 @@ import 'package:flutter/material.dart';
 class Katswiri extends StatelessWidget {
   const Katswiri({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Katswiri',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        backgroundColor: Colors.black12,
+        scaffoldBackgroundColor: const Color(0xFF000000),
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        final MaterialPageRoute pageRoute;
+        final path = settings.name as String;
+
+        switch (path) {
+          case ExploreJobsScreen.route:
+            pageRoute = MaterialPageRoute(
+              builder: (context) => ExploreJobsScreen(),
+            );
+            break;
+          case SavedJobsScreen.route:
+            pageRoute = MaterialPageRoute(
+              builder: (context) => SavedJobsScreen(),
+            );
+            break;
+          case JobDetailScreen.route:
+            pageRoute = MaterialPageRoute(
+              builder: (context) => JobDetailScreen(),
+            );
+            break;
+          default:
+            pageRoute = MaterialPageRoute(
+              builder: (context) => ExploreJobsScreen(),
+            );
+        }
+
+        return pageRoute;
+      },
     );
   }
 }
