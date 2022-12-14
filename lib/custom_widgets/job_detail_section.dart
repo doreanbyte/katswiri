818490c6f61203cc9816df6c58ad17c6de70c050
ngomile <ngomile932@gmail.com>
2022-12-14 11:04:54 +0200

    Move header section to separate widget

--- lib/custom_widgets/job_detail_section.dart
+++ lib/custom_widgets/job_detail_section.dart
@@ -65,146 +65,7 @@ class _JobDetailSectionState extends State<JobDetailSection>
       crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisSize: MainAxisSize.max,
       children: [
-        Container(
-          padding: const EdgeInsets.all(8.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8.0),
-            color: Colors.black87,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.1),
-                spreadRadius: 0.5,
-                blurRadius: 1,
-                offset: const Offset(1, 0),
-              ),
-            ],
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Column(
-                      textDirection: TextDirection.ltr,
-                      children: [
-                        SizedBox(
-                          width: 150.0,
-                          height: 150.0,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Hero(
-                              tag: _job.url,
-                              child: Image.network(
-                                _job.logo,
-                                fit: BoxFit.contain,
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 12.0,
-                        ),
-                        Text(
-                          _job.companyName,
-                          maxLines: 5,
-                          style: const TextStyle(
-                            color: Colors.white70,
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w500,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            const Icon(
-                              Icons.access_time_filled,
-                              color: Colors.white,
-                            ),
-                            const SizedBox(
-                              width: 8.0,
-                            ),
-                            Expanded(
-                              child: Text(_job.posted),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 16.0,
-                        ),
-                        Row(
-                          children: [
-                            const Icon(
-                              Icons.business,
-                              color: Colors.white,
-                            ),
-                            const SizedBox(
-                              width: 8.0,
-                            ),
-                            Expanded(
-                              child: Text(_job.type),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 16.0,
-                        ),
-                        GestureDetector(
-                          //TODO: Implement on gesture for location
-                          onTapUp: (_) {},
-                          child: Row(
-                            children: [
-                              const Icon(
-                                Icons.location_pin,
-                                color: Colors.blue,
-                              ),
-                              const SizedBox(
-                                width: 8.0,
-                              ),
-                              Expanded(
-                                child: Text(
-                                  _job.location,
-                                  style: const TextStyle(
-                                    color: Colors.blue,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              Column(
-                children: [
-                  Text(
-                    _job.position,
-                    maxLines: 5,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.w500,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
+        _JobDetailHeader(_job),
         const Divider(
           height: 1.0,
           color: Colors.grey,
@@ -229,4 +90,154 @@ class _JobDetailSectionState extends State<JobDetailSection>
   void _onData(Job event) {}
 
   void _onError(Object error, StackTrace stackTrace) {}
+}
+
+class _JobDetailHeader extends StatelessWidget {
+  const _JobDetailHeader(this.job);
+
+  final Job job;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        color: Colors.black87,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            spreadRadius: 0.5,
+            blurRadius: 1,
+            offset: const Offset(1, 0),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  textDirection: TextDirection.ltr,
+                  children: [
+                    SizedBox(
+                      width: 150.0,
+                      height: 150.0,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Hero(
+                          tag: job.url,
+                          child: Image.network(
+                            job.logo,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12.0,
+                    ),
+                    Text(
+                      job.companyName,
+                      maxLines: 5,
+                      style: const TextStyle(
+                        color: Colors.white70,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.access_time_filled,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(
+                          width: 8.0,
+                        ),
+                        Expanded(
+                          child: Text(job.posted),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.business,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(
+                          width: 8.0,
+                        ),
+                        Expanded(
+                          child: Text(job.type),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    GestureDetector(
+                      //TODO: Implement on gesture for location
+                      onTapUp: (_) {},
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.location_pin,
+                            color: Colors.blue,
+                          ),
+                          const SizedBox(
+                            width: 8.0,
+                          ),
+                          Expanded(
+                            child: Text(
+                              job.location,
+                              style: const TextStyle(
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 24.0,
+          ),
+          Column(
+            children: [
+              Text(
+                job.position,
+                maxLines: 5,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w500,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
