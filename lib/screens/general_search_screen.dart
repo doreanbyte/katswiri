efe9b8a5445005df77aacf521c72a26384e4451b
ngomile <ngomile932@gmail.com>
2022-12-16 11:21:27 +0200

    Create general_search_screen stateful widget

---
+++ lib/screens/general_search_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GeneralSearchScreen extends StatefulWidget {
+  const GeneralSearchScreen({super.key});
+
+  static const route = '/general_search';
+
+  @override
+  State<GeneralSearchScreen> createState() => _GeneralSearchScreenState();
+}
+
+class _GeneralSearchScreenState extends State<GeneralSearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
