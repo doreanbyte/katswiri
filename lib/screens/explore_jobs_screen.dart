95264f5b3d5d066dd8aa4d3565a1b10f901acc02
ngomile <ngomile932@gmail.com>
2023-02-06 23:21:57 +0200

    [explore_jobs_screen] Create leading section

--- lib/screens/explore_jobs_screen.dart
+++ lib/screens/explore_jobs_screen.dart
@@ -26,29 +26,86 @@ class _ExploreJobsScreenState extends State<ExploreJobsScreen>
     super.dispose();
   }
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: const Text('Explore'),
+  //       backgroundColor: Colors.transparent,
+  //       bottom: TabBar(
+  //         labelColor: Colors.blue,
+  //         unselectedLabelColor: Colors.white70,
+  //         indicatorColor: Colors.blue,
+  //         controller: _tabController,
+  //         tabs: _sources
+  //             .map<Widget>((source) => Tab(
+  //                   text: source.title,
+  //                 ))
+  //             .toList(),
+  //       ),
+  //     ),
+  //     body: TabBarView(
+  //       controller: _tabController,
+  //       children: _sources
+  //           .map<Widget>((source) => JobListRetriever(source: source))
+  //           .toList(),
+  //     ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Explore'),
-        backgroundColor: Colors.transparent,
-        bottom: TabBar(
-          labelColor: Colors.blue,
-          unselectedLabelColor: Colors.white70,
-          indicatorColor: Colors.blue,
-          controller: _tabController,
-          tabs: _sources
-              .map<Widget>((source) => Tab(
-                    text: source.title,
-                  ))
-              .toList(),
-        ),
+    return SingleChildScrollView(
+      child: Stack(
+        children: const [
+          ExploreLeadSection(),
+        ],
       ),
-      body: TabBarView(
-        controller: _tabController,
-        children: _sources
-            .map<Widget>((source) => JobListRetriever(source: source))
-            .toList(),
+    );
+  }
+}
+
+class ExploreLeadSection extends StatelessWidget {
+  const ExploreLeadSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: const [
+              Icon(
+                Icons.explore_rounded,
+                color: Colors.white70,
+                size: 36.0,
+              ),
+              SizedBox(
+                width: 12.0,
+              ),
+              Text(
+                'Explore',
+                style: TextStyle(
+                  fontSize: 42.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          const Text(
+            'Browse and Discover Jobs from Multiple Sources',
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.w400,
+              color: Colors.grey,
+            ),
+          ),
+        ],
       ),
     );
   }
