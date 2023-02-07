cebac70d09d758079b69b9b2fea9e6829571a8d2
ngomile <ngomile932@gmail.com>
2023-02-07 03:05:53 +0200

    [job_detail_screen] Begin gradual refactoring of job detail screen

--- lib/screens/job_detail_screen.dart
+++ lib/screens/job_detail_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:katswiri/custom_widgets/custom_widgets.dart';
 import 'package:katswiri/models/models.dart';
 import 'package:katswiri/sources/sources.dart';
 
-class JobDetailScreen extends StatefulWidget {
+class JobDetailScreen extends StatelessWidget {
   const JobDetailScreen({
     super.key,
     required this.job,
@@ -16,10 +15,74 @@ class JobDetailScreen extends StatefulWidget {
   static const route = '/job_detail';
 
   @override
-  State<JobDetailScreen> createState() => _JobDetailScreenState();
+  Widget build(BuildContext context) {
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text(widget.job.position),
+    //     elevation: 0,
+    //     backgroundColor: Colors.transparent,
+    //     leading: IconButton(
+    //       onPressed: () => Navigator.of(context).pop(),
+    //       icon: const Icon(
+    //         Icons.chevron_left_rounded,
+    //         size: 32.0,
+    //       ),
+    //     ),
+    //     bottom: TabBar(
+    //       labelColor: Colors.blue,
+    //       unselectedLabelColor: Colors.white70,
+    //       indicatorColor: Colors.blue,
+    //       controller: _tabController,
+    //       tabs: const [
+    //         Tab(text: 'Description'),
+    //         Tab(text: 'Related'),
+    //       ],
+    //     ),
+    //   ),
+    //   body: SafeArea(
+    //     child: TabBarView(
+    //       controller: _tabController,
+    //       children: [
+    //         JobDetailSection(
+    //           job: widget.job,
+    //           source: widget.source,
+    //         ),
+    //         JobListRetriever(
+    //           source: widget.source,
+    //           arguments: {
+    //             'position': widget.job.position,
+    //           },
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
+    return Scaffold(
+      body: SafeArea(
+        child: JobDetailContent(
+          job: job,
+          source: source,
+        ),
+      ),
+    );
+  }
 }
 
-class _JobDetailScreenState extends State<JobDetailScreen>
+class JobDetailContent extends StatefulWidget {
+  const JobDetailContent({
+    super.key,
+    required this.job,
+    required this.source,
+  });
+
+  final Job job;
+  final Source source;
+
+  @override
+  State<JobDetailContent> createState() => _JobDetailContentState();
+}
+
+class _JobDetailContentState extends State<JobDetailContent>
     with TickerProviderStateMixin {
   late final _tabController = TabController(
     length: 2,
@@ -29,52 +92,11 @@ class _JobDetailScreenState extends State<JobDetailScreen>
   @override
   void dispose() {
     _tabController.dispose();
-
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.job.position),
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        leading: IconButton(
-          onPressed: () => Navigator.of(context).pop(),
-          icon: const Icon(
-            Icons.chevron_left_rounded,
-            size: 32.0,
-          ),
-        ),
-        bottom: TabBar(
-          labelColor: Colors.blue,
-          unselectedLabelColor: Colors.white70,
-          indicatorColor: Colors.blue,
-          controller: _tabController,
-          tabs: const [
-            Tab(text: 'Description'),
-            Tab(text: 'Related'),
-          ],
-        ),
-      ),
-      body: SafeArea(
-        child: TabBarView(
-          controller: _tabController,
-          children: [
-            JobDetailSection(
-              job: widget.job,
-              source: widget.source,
-            ),
-            JobListRetriever(
-              source: widget.source,
-              arguments: {
-                'position': widget.job.position,
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+    return Container();
   }
 }
