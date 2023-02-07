d4dc828aba27a0fa6abce29466f1eaf88ef5a475
ngomile <ngomile932@gmail.com>
2023-02-07 02:18:21 +0200

    [job_model_widgets] Finish refactor job tile by separating it into separate widgets

--- lib/custom_widgets/job_model_widgets.dart
+++ lib/custom_widgets/job_model_widgets.dart
@@ -114,187 +114,21 @@ class _JobTileState extends State<JobTile> {
 
   @override
   Widget build(BuildContext context) {
-    // return DefaultTextStyle(
-    //   style: const TextStyle(
-    //     color: Colors.white,
-    //   ),
-    //   child: GestureDetector(
-    //     onTapUp: (_) => Navigator.pushNamed(
-    //       context,
-    //       JobDetailScreen.route,
-    //       arguments: {
-    //         'job': _job,
-    //         'source': widget.source,
-    //       },
-    //     ),
-    //     child: Container(
-    //       padding: const EdgeInsets.all(4.0),
-    //       margin: const EdgeInsets.only(bottom: 24.0),
-    //       decoration: BoxDecoration(
-    //         borderRadius: BorderRadius.circular(8.0),
-    //         color: Colors.black87,
-    //         boxShadow: [
-    //           BoxShadow(
-    //             color: Colors.grey.withOpacity(0.1),
-    //             spreadRadius: 0.5,
-    //             blurRadius: 1,
-    //             offset: const Offset(1, 0),
-    //           ),
-    //         ],
-    //       ),
-    //       child: Column(
-    //         mainAxisSize: MainAxisSize.max,
-    //         children: [
-    //           Row(
-    //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //             crossAxisAlignment: CrossAxisAlignment.start,
-    //             children: [
-    //               SizedBox(
-    //                 width: 70.0,
-    //                 height: 70.0,
-    //                 child: ClipRRect(
-    //                   borderRadius: BorderRadius.circular(8.0),
-    //                   child: Hero(
-    //                     tag: _job.url,
-    //                     child: Image.network(
-    //                       _job.logo,
-    //                       fit: BoxFit.contain,
-    //                     ),
-    //                   ),
-    //                 ),
-    //               ),
-    //               const SizedBox(width: 16.0),
-    //               Flexible(
-    //                 child: Column(
-    //                   crossAxisAlignment: CrossAxisAlignment.start,
-    //                   children: [
-    //                     Text(
-    //                       _job.position,
-    //                       style: const TextStyle(
-    //                         fontSize: 18.0,
-    //                         fontWeight: FontWeight.w500,
-    //                         overflow: TextOverflow.ellipsis,
-    //                       ),
-    //                       maxLines: 2,
-    //                     ),
-    //                     const SizedBox(
-    //                       height: 12.0,
-    //                     ),
-    //                     Row(
-    //                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //                       crossAxisAlignment: CrossAxisAlignment.start,
-    //                       children: [
-    //                         Expanded(
-    //                           child: Text(
-    //                             _job.companyName,
-    //                             maxLines: 2,
-    //                             style: const TextStyle(
-    //                               color: Color(0xFFFFFFFF),
-    //                               fontSize: 16.0,
-    //                               fontWeight: FontWeight.w300,
-    //                               overflow: TextOverflow.ellipsis,
-    //                             ),
-    //                           ),
-    //                         ),
-    //                         const SizedBox(
-    //                           width: 8.0,
-    //                         ),
-    //                         //TODO: Add onTapUp gesture detector to location text
-    //                         // to direct user to page showing jobs matching this location
-    //                         Expanded(
-    //                           child: Text(
-    //                             _job.location,
-    //                             maxLines: 1,
-    //                             overflow: TextOverflow.ellipsis,
-    //                             style: const TextStyle(
-    //                               color: Colors.blue,
-    //                               fontSize: 16.0,
-    //                               fontWeight: FontWeight.w500,
-    //                             ),
-    //                           ),
-    //                         ),
-    //                       ],
-    //                     ),
-    //                   ],
-    //                 ),
-    //               ),
-    //             ],
-    //           ),
-    //           const SizedBox(
-    //             height: 16.0,
-    //           ),
-    //           Row(
-    //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //             children: [
-    //               Expanded(
-    //                 child: Text(
-    //                   '${_job.type}  \u2022  ${_job.posted}',
-    //                   style: const TextStyle(
-    //                     color: Colors.grey,
-    //                     fontSize: 16.0,
-    //                     fontWeight: FontWeight.w500,
-    //                   ),
-    //                 ),
-    //               ),
-    //               Row(
-    //                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    //                 children: [
-    //                   GestureDetector(
-    //                     //TODO: Implement saving jobs
-    //                     onTap: () {
-    //                       setState(() {
-    //                         _job = Job(
-    //                           logo: _job.logo,
-    //                           position: _job.position,
-    //                           companyName: _job.companyName,
-    //                           location: _job.location,
-    //                           type: _job.type,
-    //                           posted: _job.posted,
-    //                           url: _job.url,
-    //                           description: _job.description,
-    //                           saved: !_job.saved,
-    //                         );
-    //                       });
-    //                     },
-    //                     child: _job.saved
-    //                         ? const Icon(
-    //                             Icons.bookmark,
-    //                             color: Colors.blue,
-    //                             size: 28.0,
-    //                           )
-    //                         : Icon(
-    //                             Icons.bookmark_outline,
-    //                             color: Colors.grey.shade600,
-    //                             size: 28.0,
-    //                           ),
-    //                   ),
-    //                   IconButton(
-    //                     onPressed: () async {
-    //                       await Share.share(
-    //                         _job.url,
-    //                         subject: _job.position,
-    //                       );
-    //                     },
-    //                     icon: const Icon(
-    //                       Icons.share,
-    //                       color: Colors.blue,
-    //                       size: 28.0,
-    //                     ),
-    //                   ),
-    //                 ],
-    //               ),
-    //             ],
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //   ),
-    // );
     return DefaultTextStyle(
       style: const TextStyle(
         color: Colors.white,
       ),
-      child: JobTileContent(job: _job),
+      child: GestureDetector(
+        onTapUp: (_) => Navigator.pushNamed(
+          context,
+          JobDetailScreen.route,
+          arguments: {
+            'job': _job,
+            'source': widget.source,
+          },
+        ),
+        child: JobTileContent(job: _job),
+      ),
     );
   }
 }
@@ -308,17 +142,26 @@ class JobTileContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(8.0),
-      margin: const EdgeInsets.only(bottom: 12.0),
+      margin: const EdgeInsets.only(bottom: 8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [
-              JobTileImage(job: job),
+              Hero(
+                tag: job.url,
+                child: JobTileImage(job: job),
+              ),
+              const SizedBox(
+                width: 16.0,
+              ),
+              Expanded(
+                child: JobTileDetail(job: job),
+              ),
             ],
           ),
           const SizedBox(
-            height: 2.0,
+            height: 8.0,
           ),
           JobTileActions(job),
         ],
@@ -344,7 +187,7 @@ class JobTileImage extends StatelessWidget {
       height: size,
       width: size,
       decoration: BoxDecoration(
-        color: Colors.black87,
+        color: Colors.white,
         shape: BoxShape.circle,
         image: DecorationImage(
           fit: BoxFit.fill,
@@ -357,6 +200,54 @@ class JobTileImage extends StatelessWidget {
   }
 }
 
+class JobTileDetail extends StatelessWidget {
+  const JobTileDetail({super.key, required this.job});
+
+  final Job job;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          job.position,
+          style: const TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w700,
+            overflow: TextOverflow.ellipsis,
+          ),
+          maxLines: 2,
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        Text(
+          job.companyName,
+          style: const TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+            overflow: TextOverflow.ellipsis,
+            color: Colors.white54,
+          ),
+          maxLines: 2,
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        Text(
+          '${job.type}  \u2022  ${job.posted}',
+          style: const TextStyle(
+            color: Colors.grey,
+            fontWeight: FontWeight.w500,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class JobTileActions extends StatelessWidget {
   const JobTileActions(this.job, {super.key});
 
@@ -365,8 +256,19 @@ class JobTileActions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
+        OutlinedButton.icon(
+          //TODO: Implement onPressed
+          onPressed: () {},
+          icon: const Icon(
+            Icons.location_city,
+          ),
+          label: Text(
+            job.location,
+            maxLines: 1,
+          ),
+        ),
         IconButton(
           onPressed: () async {
             await Share.share(
