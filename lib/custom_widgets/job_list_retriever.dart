602b55956cb36b739fabdabf69ddee9c326433ba
ngomile <ngomile932@gmail.com>
2023-02-09 00:57:12 +0200

    [job_list_retriever] Implement sorting of Job list items

--- lib/custom_widgets/job_list_retriever.dart
+++ lib/custom_widgets/job_list_retriever.dart
@@ -116,6 +116,7 @@ class _JobListRetrieverState extends State<JobListRetriever>
 
   void _onData(List<Job> jobs) {
     _jobs.addAll(jobs);
+    _jobs.sort(((a, b) => _postedDate(a).compareTo(_postedDate(b))));
 
     setState(() {
       _page++;
@@ -139,5 +140,47 @@ class _JobListRetrieverState extends State<JobListRetriever>
     _jobs.clear();
     _page = 1;
     _getJobs();
+  }
+}
+
+DateTime _postedDate(Job job) {
+  var posted = job.posted.toLowerCase();
+  posted = posted.replaceAll('posted', '')
+    ..replaceAll('ago', '')
+    ..trim();
+
+  final period = int.tryParse(posted.split(' ').first);
+  DateTime now = DateTime.now();
+
+  if (period == null) {
+    return now;
+  }
+
+  if (posted.contains('second')) {
+    return now.subtract(
+      Duration(seconds: period),
+    );
+  } else if (posted.contains('minute')) {
+    return now.subtract(
+      Duration(minutes: period),
+    );
+  } else if (posted.contains('hour')) {
+    return now.subtract(
+      Duration(hours: period),
+    );
+  } else if (posted.contains('day')) {
+    return now.subtract(
+      Duration(days: period),
+    );
+  } else if (posted.contains('month')) {
+    return now.subtract(
+      Duration(days: period * 30),
+    );
+  } else if (posted.contains('year')) {
+    return now.subtract(
+      Duration(days: period * 365),
+    );
+  } else {
+    return now;
   }
 }
