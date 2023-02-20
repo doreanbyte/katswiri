7f6b0d0c72d6466e687dbcdf0ffae2e0cd0c4cf8
ngomile <ngomile932@gmail.com>
2023-02-20 15:46:29 +0200

    [job_detail_screen] Add action icon buttons to LeadingSection widget

--- lib/screens/job_detail_screen.dart
+++ lib/screens/job_detail_screen.dart
@@ -1,6 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:katswiri/models/models.dart';
 import 'package:katswiri/sources/sources.dart';
+import 'package:share_plus/share_plus.dart';
 
 class JobDetailScreen extends StatefulWidget {
   const JobDetailScreen({
@@ -21,47 +23,6 @@ class JobDetailScreen extends StatefulWidget {
 class _JobDetailScreenState extends State<JobDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     title: Text(widget.job.position),
-    //     elevation: 0,
-    //     backgroundColor: Colors.transparent,
-    //     leading: IconButton(
-    //       onPressed: () => Navigator.of(context).pop(),
-    //       icon: const Icon(
-    //         Icons.chevron_left_rounded,
-    //         size: 32.0,
-    //       ),
-    //     ),
-    //     bottom: TabBar(
-    //       labelColor: Colors.blue,
-    //       unselectedLabelColor: Colors.white70,
-    //       indicatorColor: Colors.blue,
-    //       controller: _tabController,
-    //       tabs: const [
-    //         Tab(text: 'Description'),
-    //         Tab(text: 'Related'),
-    //       ],
-    //     ),
-    //   ),
-    //   body: SafeArea(
-    //     child: TabBarView(
-    //       controller: _tabController,
-    //       children: [
-    //         JobDetailSection(
-    //           job: widget.job,
-    //           source: widget.source,
-    //         ),
-    //         JobListRetriever(
-    //           source: widget.source,
-    //           arguments: {
-    //             'position': widget.job.position,
-    //           },
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
     return Scaffold(
       body: SafeArea(
         child: JobDetailContent(
@@ -96,26 +57,16 @@ class _JobDetailContentState extends State<JobDetailContent>
 
   @override
   void dispose() {
-    super.dispose();
     _tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return NestedScrollView(
-      physics: NeverScrollableScrollPhysics(),
-      headerSliverBuilder: (context, isScrolled) => [
-        SliverAppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Colors.transparent,
-          collapsedHeight: 350,
-          expandedHeight: 350,
-          flexibleSpace: LeadingSection(
-            job: widget.job,
-          ),
-        ),
+    return Column(
+      children: [
+        LeadingSection(job: widget.job),
       ],
-      body: Container(),
     );
   }
 }
@@ -130,48 +81,63 @@ class LeadingSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          opacity: .1,
-          fit: BoxFit.cover,
-          image: NetworkImage(job.logo),
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: const Icon(
-              Icons.chevron_left_sharp,
-              size: 38.0,
-              color: Colors.white,
-            ),
-          ),
-          const SizedBox(
-            height: 8.0,
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Text(
-                    job.position,
-                    style: const TextStyle(
-                      fontSize: 38.0,
-                      fontWeight: FontWeight.w900,
-                    ),
-                    textAlign: TextAlign.center,
+    return Card(
+      color: const Color.fromARGB(38, 96, 96, 96),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * .30,
+        child: Stack(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.chevron_left_outlined,
+                    color: Colors.blue,
+                    size: 28.0,
                   ),
-                ],
-              ),
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () async {
+                    await Share.share(
+                      job.url,
+                      subject: job.position,
+                    );
+                  },
+                  icon: const Icon(
+                    Icons.share_sharp,
+                    color: Colors.blue,
+                    size: 28.0,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.bookmark_add_outlined,
+                    color: Colors.blue,
+                    size: 28.0,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.public_outlined,
+                    color: Colors.blue,
+                    size: 28.0,
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
