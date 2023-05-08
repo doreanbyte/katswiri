63d4249d7ba79010d1733ff4934921ed1b76ec3d
ngomile <ngomile932@gmail.com>
2023-05-08 13:48:43 +0200

    [main] Call WidgetsFlutterBinding.ensureInitialized() to fix error

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:katswiri/db_manager.dart' show DBManager;
 import 'package:katswiri/screens/screens.dart' show Katswiri;
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await DBManager.instance.database;
   runApp(const Katswiri());
 }
