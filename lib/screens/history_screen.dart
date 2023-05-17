1c66b285af4eec9706ee91d36cd8414cd6c1ef49
ngomile <ngomile932@gmail.com>
2023-05-17 21:16:25 +0200

    [history_screen] Implemented HistoryListRetriever

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:katswiri/custom_widgets/custom_widgets.dart';
+import 'package:katswiri/models/models.dart';
+import 'package:katswiri/repository/repository.dart';
+import 'package:katswiri/sources/sources.dart';
 
 class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
@@ -13,7 +19,136 @@ class _HistoryScreenState extends State<HistoryScreen> {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('History'),
+      child: HistoryListRetriever(),
     );
+  }
+}
+
+/// [HistoryListRetriever] retrieves the list of jobs that happen to have matching
+/// ids in the history table. A [StreamBuilder] is used in conjunction with
+/// [StreamController] listening to the stream and waiting for a list of [Job]
+/// elements.
+class HistoryListRetriever extends StatefulWidget {
+  const HistoryListRetriever({super.key});
+
+  @override
+  State<HistoryListRetriever> createState() => _HistoryListRetrieverState();
+}
+
+class _HistoryListRetrieverState extends State<HistoryListRetriever> {
+  late final StreamController<List<Job>> _streamController =
+      StreamController.broadcast();
+  final List<Job> _jobs = [];
+
+  bool _loading = true;
+  bool _hasError = false;
+  String _errMsg = '';
+
+  @override
+  void initState() {
+    _streamController.stream.listen(
+      _onData,
+      onError: _onError,
+    );
+    super.initState();
+    _getHistory();
+  }
+
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<Job>>(
+      stream: _streamController.stream,
+      builder: _builder,
+    );
+  }
+
+  Widget _builder(BuildContext context, _) {
+    final List<Widget> jobTileList = _jobs
+        .map<Widget>((job) => JobTile(
+              job: job,
+              source: getSources().firstWhere(
+                (source) => job.url.contains(source.host),
+              ),
+            ))
+        .toList();
+
+    if (_loading) {
+      jobTileList.add(
+        const Spinner(),
+      );
+    }
+
+    if (_hasError) {
+      jobTileList.add(
+        ErrorButton(
+          errorMessage: _errMsg,
+          onRetryPressed: _onRetryPressed,
+        ),
+      );
+    }
+
+    if (_jobs.isEmpty && !_loading && _errMsg.isEmpty) {
+      const Center(
+        child: Text('No Recently Viewed Jobs'),
+      );
+    }
+
+    return RefreshIndicator(
+      backgroundColor: Colors.black,
+      onRefresh: _onRefresh,
+      child: ListView.builder(
+        addAutomaticKeepAlives: false,
+        addRepaintBoundaries: false,
+        padding: const EdgeInsets.only(top: 4.0),
+        itemBuilder: (context, index) => jobTileList[index],
+        itemCount: jobTileList.length,
+      ),
+    );
+  }
+
+  void _getHistory() async {
+    final jobs = await JobHistoryRepo.viewedJobs();
+    _streamController.sink.add(jobs);
+  }
+
+  void _onData(List<Job> jobs) {
+    _jobs.addAll(jobs);
+
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  void _onError(Object error, StackTrace stackTrace) {
+    setState(() {
+      _loading = false;
+      _hasError = true;
+      _errMsg = error.toString();
+    });
+  }
+
+  void _onRetryPressed() {
+    setState(() {
+      _hasError = false;
+      _errMsg = '';
+      _loading = true;
+    });
+
+    _getHistory();
+  }
+
+  Future<void> _onRefresh() async {
+    setState(() {
+      _jobs.clear();
+      _loading = true;
+    });
+
+    _getHistory();
   }
 }
