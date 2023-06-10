da7b37dc43e6f9817253b0e5f271bcad3603dd67
ngomile <ngomile932@gmail.com>
2023-06-10 11:58:33 +0200

    [app_theme] Simplify creation of theme data

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -5,106 +5,114 @@ enum AppTheme {
   greenDark,
 }
 
-ThemeData buildAppTheme(BuildContext context, AppTheme appTheme) =>
-    switch (appTheme) {
-      AppTheme.greenLight => ThemeData(
-          splashColor: Colors.transparent,
-          highlightColor: Colors.transparent,
-          brightness: Brightness.light,
-          backgroundColor: Colors.white,
-          scaffoldBackgroundColor: Colors.white,
-          primarySwatch: Colors.green,
-          textTheme: Theme.of(context).textTheme.apply(
-                bodyColor: Colors.black,
-                displayColor: Colors.black,
-              ),
-          iconTheme: const IconThemeData(
-            color: Colors.green,
+ThemeData buildAppTheme(
+  BuildContext context,
+  AppTheme appTheme,
+) =>
+    ThemeData(
+      splashColor: switch (appTheme) {
+        AppTheme.greenLight || AppTheme.greenDark => Colors.transparent,
+      },
+      highlightColor: switch (appTheme) {
+        AppTheme.greenLight || AppTheme.greenDark => Colors.transparent,
+      },
+      brightness: switch (appTheme) {
+        AppTheme.greenLight => Brightness.light,
+        AppTheme.greenDark => Brightness.dark,
+      },
+      backgroundColor: switch (appTheme) {
+        AppTheme.greenLight => Colors.white,
+        AppTheme.greenDark => Colors.black12,
+      },
+      scaffoldBackgroundColor: switch (appTheme) {
+        AppTheme.greenLight => Colors.white,
+        AppTheme.greenDark => Colors.black87,
+      },
+      primarySwatch: switch (appTheme) {
+        AppTheme.greenLight || AppTheme.greenDark => Colors.green,
+      },
+      textTheme: Theme.of(context).textTheme.apply(
+            bodyColor: switch (appTheme) {
+              AppTheme.greenLight => Colors.black,
+              AppTheme.greenDark => Colors.white,
+            },
+            displayColor: switch (appTheme) {
+              AppTheme.greenLight => Colors.black,
+              AppTheme.greenDark => Colors.white,
+            },
           ),
-          primaryIconTheme: const IconThemeData(
-            color: Colors.green,
-          ),
-          primaryColor: Colors.green,
-          iconButtonTheme: IconButtonThemeData(
-            style: ButtonStyle(
-              foregroundColor:
-                  MaterialStateProperty.all<Color>(Colors.transparent),
-              overlayColor:
-                  MaterialStateProperty.all<Color>(Colors.transparent),
-            ),
-          ),
-          textSelectionTheme: const TextSelectionThemeData(
-            cursorColor: Colors.black,
-          ),
-          cardTheme: const CardTheme(
-            color: Colors.white,
-          ),
-          secondaryHeaderColor: Colors.grey,
-          inputDecorationTheme: const InputDecorationTheme(
-            filled: true,
-            fillColor: Color.fromARGB(255, 15, 9, 9),
-          ),
-          tabBarTheme: const TabBarTheme(
-            unselectedLabelColor: Colors.white70,
-            labelColor: Colors.green,
-          ),
-          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-            backgroundColor: Color(0xFFF0F0F0),
-            unselectedItemColor: Colors.grey,
-          ),
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.transparent,
-          ),
-          dialogTheme: const DialogTheme(
-            backgroundColor: Colors.white,
-          ),
+      iconTheme: IconThemeData(
+        color: switch (appTheme) {
+          AppTheme.greenLight || AppTheme.greenDark => Colors.green,
+        },
+      ),
+      primaryIconTheme: IconThemeData(
+        color: switch (appTheme) {
+          AppTheme.greenLight || AppTheme.greenDark => Colors.green,
+        },
+      ),
+      primaryColor: switch (appTheme) {
+        AppTheme.greenLight || AppTheme.greenDark => Colors.green,
+      },
+      iconButtonTheme: IconButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: switch (appTheme) {
+            AppTheme.greenLight =>
+              MaterialStateProperty.all<Color>(Colors.transparent),
+            AppTheme.greenDark =>
+              MaterialStateProperty.all<Color>(Colors.green),
+          },
+          overlayColor: switch (appTheme) {
+            AppTheme.greenLight =>
+              MaterialStateProperty.all<Color>(Colors.transparent),
+            AppTheme.greenDark =>
+              MaterialStateProperty.all<Color>(Colors.green),
+          },
         ),
-      AppTheme.greenDark => ThemeData(
-          splashColor: Colors.transparent,
-          highlightColor: Colors.transparent,
-          brightness: Brightness.dark,
-          backgroundColor: Colors.black12,
-          scaffoldBackgroundColor: Colors.black87,
-          primarySwatch: Colors.green,
-          textTheme: Theme.of(context).textTheme.apply(
-                bodyColor: Colors.white,
-                displayColor: Colors.white,
-              ),
-          iconTheme: const IconThemeData(
-            color: Colors.green,
-          ),
-          primaryColor: Colors.green,
-          iconButtonTheme: IconButtonThemeData(
-            style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all<Color>(Colors.green),
-              overlayColor:
-                  MaterialStateProperty.all<Color>(Colors.transparent),
-            ),
-          ),
-          textSelectionTheme: const TextSelectionThemeData(
-            cursorColor: Colors.white,
-          ),
-          cardTheme: const CardTheme(
-            color: Color.fromARGB(96, 64, 64, 64),
-          ),
-          secondaryHeaderColor: Colors.grey,
-          inputDecorationTheme: const InputDecorationTheme(
-            filled: true,
-            fillColor: Colors.white,
-          ),
-          tabBarTheme: const TabBarTheme(
-            unselectedLabelColor: Colors.white70,
-            labelColor: Colors.green,
-          ),
-          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-            backgroundColor: Color(0xFF000000),
-            unselectedItemColor: Colors.grey,
-          ),
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.transparent,
-          ),
-          dialogTheme: const DialogTheme(
-            backgroundColor: Colors.black,
-          ),
-        )
-    };
+      ),
+      textSelectionTheme: TextSelectionThemeData(
+        cursorColor: switch (appTheme) {
+          AppTheme.greenLight => Colors.black,
+          AppTheme.greenDark => Colors.white,
+        },
+      ),
+      cardTheme: CardTheme(
+        color: switch (appTheme) {
+          AppTheme.greenLight => Colors.white,
+          AppTheme.greenDark => const Color.fromARGB(96, 64, 64, 64),
+        },
+      ),
+      secondaryHeaderColor: switch (appTheme) {
+        AppTheme.greenLight || AppTheme.greenDark => Colors.grey,
+      },
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: switch (appTheme) {
+          AppTheme.greenLight => const Color.fromARGB(255, 15, 9, 9),
+          AppTheme.greenDark => Colors.white,
+        },
+      ),
+      tabBarTheme: TabBarTheme(
+        unselectedLabelColor: switch (appTheme) {
+          AppTheme.greenLight => Colors.grey,
+          AppTheme.greenDark => Colors.white70,
+        },
+        labelColor: Colors.green,
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: switch (appTheme) {
+          AppTheme.greenLight => const Color(0xFFF0F0F0),
+          AppTheme.greenDark => const Color(0xFF000000),
+        },
+        unselectedItemColor: Colors.grey,
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.transparent,
+      ),
+      dialogTheme: DialogTheme(
+        backgroundColor: switch (appTheme) {
+          AppTheme.greenLight => Colors.white,
+          AppTheme.greenDark => Colors.black,
+        },
+      ),
+    );
