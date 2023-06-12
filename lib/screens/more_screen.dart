7542ce535b712c4d88213845b6db809f6b2ed916
ngomile <ngomile932@gmail.com>
2023-06-12 21:25:26 +0200

    [more_screen] Created more_screen file

---
+++ lib/screens/more_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class MoreScreen extends StatelessWidget {
+  const MoreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
