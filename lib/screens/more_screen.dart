3ad9dc873e7bf7e18f4ee1cd246ea23d81ca85ab
ngomile <ngomile932@gmail.com>
2023-06-13 22:36:56 +0200

    [more_screen] Implement widget to toggle between job views

--- lib/screens/more_screen.dart
+++ lib/screens/more_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:katswiri/bloc/bloc.dart';
 import 'package:katswiri/custom_widgets/custom_widgets.dart'
-    show ToggleThemeButtons;
+    show ToggleThemeButtons, ToggleJobViewButtons;
 
 class MoreScreen extends StatelessWidget {
   const MoreScreen({super.key});
@@ -14,6 +14,8 @@ class MoreScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _ToggleThemeSection(),
+          SizedBox(height: 8.0),
+          _ToggleJobViewSection(),
         ],
       ),
     );
@@ -47,6 +49,38 @@ class _ToggleThemeSection extends StatelessWidget {
             child: BlocBuilder<ThemeBloc, SelectedThemeState>(
               builder: (context, state) => ToggleThemeButtons(state),
             ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ToggleJobViewSection extends StatelessWidget {
+  const _ToggleJobViewSection();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Select Preferred View',
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          color: Theme.of(context).cardTheme.color,
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            child: const ToggleJobViewButtons(),
           ),
         ),
       ],
