71f2ba7cfb671af30dfaf52740857f2d31ea18e3
ngomile <ngomile932@gmail.com>
2023-06-13 21:52:23 +0200

    [more_screen] Added section for theme selection

--- lib/screens/more_screen.dart
+++ lib/screens/more_screen.dart
@@ -1,10 +1,55 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:katswiri/bloc/bloc.dart';
+import 'package:katswiri/custom_widgets/custom_widgets.dart'
+    show ToggleThemeButtons;
 
 class MoreScreen extends StatelessWidget {
   const MoreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _ToggleThemeSection(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ToggleThemeSection extends StatelessWidget {
+  const _ToggleThemeSection();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Theme Settings',
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          color: Theme.of(context).cardTheme.color,
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            child: BlocBuilder<ThemeBloc, SelectedThemeState>(
+              builder: (context, state) => ToggleThemeButtons(state),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
