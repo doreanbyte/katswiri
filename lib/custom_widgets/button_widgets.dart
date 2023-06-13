133e2ad75e41596b09ce859b1d425b6a42d64199
ngomile <ngomile932@gmail.com>
2023-06-13 21:51:05 +0200

    [button_widgets] Add ToggleThemeButtons widget to switch between app themes using radio buttons

--- lib/custom_widgets/button_widgets.dart
+++ lib/custom_widgets/button_widgets.dart
@@ -1,4 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:katswiri/app_theme.dart';
+import 'package:katswiri/bloc/bloc.dart';
+
+class ToggleThemeButtons extends StatefulWidget {
+  const ToggleThemeButtons(this.selectedThemeState, {super.key});
+
+  final SelectedThemeState selectedThemeState;
+
+  @override
+  State<ToggleThemeButtons> createState() => _ToggleThemeButtonsState();
+}
+
+class _ToggleThemeButtonsState extends State<ToggleThemeButtons> {
+  String? _selectedTheme;
+
+  @override
+  void initState() {
+    _selectedTheme = switch (widget.selectedThemeState) {
+      SelectedAutoTheme() => PreferredTheme.auto.name,
+      SelectedLightTheme() => PreferredTheme.light.name,
+      SelectedDarkTheme() => PreferredTheme.dark.name,
+    };
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        RadioListTile<String>(
+          title: Text(PreferredTheme.auto.name),
+          value: PreferredTheme.auto.name,
+          groupValue: _selectedTheme,
+          onChanged: (_) {
+            setState(() {
+              _selectedTheme = PreferredTheme.auto.name;
+            });
+
+            context.read<ThemeBloc>().add(
+                  ChangedThemeEvent(
+                    SelectedAutoTheme(
+                      isDark: MediaQuery.of(context).platformBrightness ==
+                          Brightness.dark,
+                    ),
+                  ),
+                );
+          },
+        ),
+        RadioListTile<String>(
+          title: Text(PreferredTheme.light.name),
+          value: PreferredTheme.light.name,
+          groupValue: _selectedTheme,
+          onChanged: (_) {
+            setState(() {
+              _selectedTheme = PreferredTheme.light.name;
+            });
+
+            context.read<ThemeBloc>().add(
+                  const ChangedThemeEvent(
+                    SelectedLightTheme(),
+                  ),
+                );
+          },
+        ),
+        RadioListTile<String>(
+          title: Text(PreferredTheme.dark.name),
+          value: PreferredTheme.dark.name,
+          groupValue: _selectedTheme,
+          onChanged: (_) {
+            setState(() {
+              _selectedTheme = PreferredTheme.dark.name;
+            });
+
+            context.read<ThemeBloc>().add(
+                  const ChangedThemeEvent(
+                    SelectedDarkTheme(),
+                  ),
+                );
+          },
+        ),
+      ],
+    );
+  }
+}
 
 class RetryButton extends StatelessWidget {
   const RetryButton({required this.onRetryPressed, super.key});
