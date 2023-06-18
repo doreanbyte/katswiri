c0d752ce722a6381987385daa30dc19725515871
ngomile <ngomile932@gmail.com>
2023-06-18 21:31:00 +0200

    [custom_widgets] Implement and add AppThemeBuilder

--- lib/custom_widgets/custom_widgets.dart
+++ lib/custom_widgets/custom_widgets.dart
@@ -1,4 +1,6 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:katswiri/app_theme.dart';
 
 export 'job_model_widgets.dart';
 export 'job_list_retriever.dart';
@@ -12,6 +14,31 @@ class Spinner extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Center(
       child: CircularProgressIndicator(),
+    );
+  }
+}
+
+class AppThemeBuilder extends StatelessWidget {
+  const AppThemeBuilder({super.key, required this.builder});
+
+  final Widget Function(
+    BuildContext context,
+    ({
+      ThemeData lightTheme,
+      ThemeData darkTheme,
+    }) appTheme,
+  ) builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return DynamicColorBuilder(
+      builder: (light, dark) => builder(
+        context,
+        fillWith(
+          light: light,
+          dark: dark,
+        ),
+      ),
     );
   }
 }
