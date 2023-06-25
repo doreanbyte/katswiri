033aef9394b20c98976c51332832cbbc7b0b879d
ngomile <ngomile932@gmail.com>
2023-06-25 20:20:41 +0200

    [webview_screen] Added progress indicator to show loading progress of web page

--- lib/screens/webview_screen.dart
+++ lib/screens/webview_screen.dart
@@ -19,12 +19,32 @@ class WebViewScreen extends StatefulWidget {
 
 class _WebViewScreenState extends State<WebViewScreen> {
   late final WebViewController controller;
+  double _progressValue = 0.0;
 
   @override
   void initState() {
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(widget.url));
+      ..loadRequest(Uri.parse(widget.url))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (progress) {
+            setState(() {
+              _progressValue = progress / 100;
+            });
+          },
+          onPageStarted: (_) {
+            setState(() {
+              _progressValue = 0.0;
+            });
+          },
+          onPageFinished: (_) {
+            setState(() {
+              _progressValue = 1.0;
+            });
+          },
+        ),
+      );
     super.initState();
   }
 
@@ -32,7 +52,17 @@ class _WebViewScreenState extends State<WebViewScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(widget.title),
+            Text(
+              widget.url,
+              style: Theme.of(context).textTheme.labelLarge,
+              overflow: TextOverflow.fade,
+            ),
+          ],
+        ),
         leading: IconButton(
           onPressed: () {
             Navigator.of(context).pop();
@@ -42,6 +72,18 @@ class _WebViewScreenState extends State<WebViewScreen> {
             color: Theme.of(context).iconTheme.color,
           ),
         ),
+        bottom: _progressValue != 1.0
+            ? PreferredSize(
+                preferredSize: const Size.fromHeight(2.0),
+                child: LinearProgressIndicator(
+                  semanticsLabel: 'Webpage Loading Progress',
+                  semanticsValue: '$_progressValue',
+                  value: _progressValue,
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                      Theme.of(context).colorScheme.primary),
+                ),
+              )
+            : null,
       ),
       body: SafeArea(
         child: WebViewWidget(controller: controller),
