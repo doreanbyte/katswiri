a217b8e9296c103c211162091e3da05e00bd4357
Noel Gomile <ngomile932@gmail.com>
2024-09-23 10:30:26 +0200

    Use JobsListBloc in JobListings and clean up code

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -3,3 +3,4 @@ export 'saved_jobs_bloc/saved_jobs_bloc.dart';
 export 'save_job_bloc/save_job_bloc.dart';
 export 'job_description_bloc/job_description_bloc.dart';
 export 'theme_bloc/theme_bloc.dart';
+export 'jobs_list_bloc/jobs_list_bloc.dart';
